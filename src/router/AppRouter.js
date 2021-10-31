5e1586cc9219806fb573f102d25bed13bd70de1c
enigma <camilo.murcia.pimentel@gmail.com>
2021-10-31 15:49:43 -0500

    add global router

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+  } from "react-router-dom";
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { BookStoreScreen } from '../components/bookStore/BookStoreScreen';
 
 export const AppRouter = () => {
     return (
-        <div>
-            
-        </div>
+        <Router>
+            <div>
+                <Switch>
+                    <Route
+                        component={LoginScreen}
+                        exact
+                        path="/login"
+                    />
+                    <Route
+                        component={BookStoreScreen}
+                        path="/"
+                    />
+                    <Redirect to="/"/>
+                </Switch>
+            </div>
+        </Router>
     )
 }
