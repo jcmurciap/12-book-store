06ad0b64773c69666a970eea5f1e7e1db4129ce1
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-02 21:09:00 -0500

    styles: Add book-store component to home screen

--- src/components/bookStore/BookStoreScreen.js
+++ src/components/bookStore/BookStoreScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { NavBar } from '../ui/NavBar';
+import { StocktakingPanel } from '../ui/StocktakingPanel';
 
 export const BookStoreScreen = () => {
     return (
         <div>
             <NavBar />
+            <StocktakingPanel />
         </div>
     );
 };
