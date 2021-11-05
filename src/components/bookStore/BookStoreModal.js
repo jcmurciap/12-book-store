0363d722c3f56aff01ebff5ea66849d0f75a708b
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-04 22:38:45 -0500

    refactor: quit component unused

--- src/components/bookStore/BookStoreModal.js
+++ src/components/bookStore/BookStoreModal.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { styled } from '@mui/system';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { Grid } from '@mui/material';
-import Paper from '@mui/material/Paper';
 import Fab from '@mui/material/Fab';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import { useDispatch, useSelector } from 'react-redux';
@@ -23,23 +21,14 @@ const style = {
     border: "1px solid #000",
 };
 
-const Item = styled(Paper)(({ theme }) => ({
-    padding: theme.spacing(1),
-    textAlign: "justify",
-    marginLeft: theme.spacing(5),
-}));
-
 export const BookStoreModal = () => {
 
     const dispatch = useDispatch();
-    
     const { modalOpen } = useSelector(state => state.ui);
     const closeModal = (event) => {
         event.preventDefault();
         dispatch(uiCloseModal());
     };
-
-    let names,genre,author,language,year,ilustrator,isbn,price;
     
     const handleClick = () => {
         console.log('something');
@@ -62,13 +51,13 @@ export const BookStoreModal = () => {
                     </Fab>
                     <Grid container spacing={0.4}>
                         <Grid item xs={12}>
-                            <TextField fullWidth id="standard-basic" label="Name" variant="standard" value={names} onChange={handleChange} size="small"/>
+                            <TextField fullWidth id="standard-basic" label="Name" variant="standard" value={'names'} onChange={handleChange} size="small"/>
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField fullWidth id="standard-basic" label="Author" variant="standard" value={author} onChange={handleChange} size="small"/>
+                            <TextField fullWidth id="standard-basic" label="Author" variant="standard" value={'author'} onChange={handleChange} size="small"/>
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField fullWidth id="standard-basic" label="Price" variant="standard" value={price} onChange={handleChange} size="small"/>
+                            <TextField fullWidth id="standard-basic" label="Price" variant="standard" value={'price'} onChange={handleChange} size="small"/>
                         </Grid>
                         <Grid item xs={12} sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}>
                             <Button color="success" variant="contained" onClick={handleClick}>
