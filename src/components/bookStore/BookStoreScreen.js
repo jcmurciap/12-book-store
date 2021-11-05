8ee29e63c22c471077e6230356ac3692a6005404
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-04 21:58:07 -0500

    refactor: change container name

--- src/components/bookStore/BookStoreScreen.js
+++ src/components/bookStore/BookStoreScreen.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { AddNewFab } from '../ui/AddNewFab';
 import { NavBar } from '../ui/NavBar';
 import { StocktakingPanel } from '../ui/StocktakingPanel';
 import { BookStoreModal } from './BookStoreModal';
 
 export const BookStoreScreen = () => {
     return (
-        <div>
+        <>
             <NavBar />
             <StocktakingPanel />
             <BookStoreModal />
-            <AddNewFab />
-        </div>
+        </>
     );
 };
