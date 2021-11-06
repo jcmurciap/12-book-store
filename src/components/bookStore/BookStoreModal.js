ee10e4efbe6e6ba15d1b97cd207257f958133995
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-06 00:00:02 -0500

    refactor: quit remove primers that will not be used

--- src/components/bookStore/BookStoreModal.js
+++ src/components/bookStore/BookStoreModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
