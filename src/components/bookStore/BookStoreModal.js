6a24f6b761dc595a7e74b61baa793d77894cd331
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-05 22:28:34 -0500

    docs: Create new books event

--- src/components/bookStore/BookStoreModal.js
+++ src/components/bookStore/BookStoreModal.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { Grid } from '@mui/material';
+import Grid from '@mui/material/Grid';
 import Fab from '@mui/material/Fab';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import { useDispatch, useSelector } from 'react-redux';
 import { uiCloseModal } from '../../actions/ui';
+import { bookStoreAddBook } from '../../actions/bookStore';
 
 const style = {
     position: "absolute",
@@ -21,46 +22,99 @@ const style = {
     border: "1px solid #000",
 };
 
+const initEvent = {id: "", name: "", author: "", price: ""};
+
 export const BookStoreModal = () => {
 
     const dispatch = useDispatch();
+    
     const { modalOpen } = useSelector(state => state.ui);
+    
     const closeModal = (event) => {
         event.preventDefault();
         dispatch(uiCloseModal());
     };
     
-    const handleClick = () => {
-        console.log('something');
+    const [formValues, setFormValues] = useState(initEvent);
+    const {id, name, author, price} = formValues;
+    
+    const onHandleChange = ({target}) => {
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value,
+        });
     };
-    const handleChange = (event) => {
-        console.log(event.target.value);
-    };   
 
+    const onHandleSubmitForm = (event) => { 
+        event.preventDefault();
+        dispatch(bookStoreAddBook(formValues));
+        dispatch(uiCloseModal());
+        setFormValues(initEvent);
+    };
+    
     return (
             <Modal open={modalOpen}>
                 <Box sx={style}>
                     <Fab 
                         aria-label="close" 
                         color="Red"
-                        onClick={closeModal} 
+                        onClick={closeModal}    
                         size="small" 
-                        sx={{ position: "absolute", marginLeft: 40, marginTop: -2 }}
+                        sx={{position:"absolute", marginLeft:40, marginTop:-2}}
                     >
                         <CloseOutlinedIcon />
                     </Fab>
                     <Grid container spacing={0.4}>
                         <Grid item xs={12}>
-                            <TextField fullWidth id="standard-basic" label="Name" variant="standard" value={'names'} onChange={handleChange} size="small"/>
+                            <TextField fullWidth 
+                                id="standard-basic" 
+                                label="Id" 
+                                name="id" 
+                                onChange={onHandleChange} 
+                                size="small"
+                                value={id}
+                                variant="standard" 
+                            />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField fullWidth id="standard-basic" label="Author" variant="standard" value={'author'} onChange={handleChange} size="small"/>
+                            <TextField fullWidth 
+                                id="standard-basic" 
+                                label="Name" 
+                                name="name" 
+                                onChange={onHandleChange} 
+                                size="small"
+                                value={name}
+                                variant="standard" 
+                            />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField fullWidth id="standard-basic" label="Price" variant="standard" value={'price'} onChange={handleChange} size="small"/>
+                            <TextField fullWidth 
+                                id="standard-basic" 
+                                label="Author" 
+                                name="author" 
+                                onChange={onHandleChange} 
+                                size="small"
+                                value={author} 
+                                variant="standard" 
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField fullWidth 
+                                id="standard-basic" 
+                                label="Price" 
+                                name="price" 
+                                onChange={onHandleChange} 
+                                size="small"
+                                value={price} 
+                                variant="standard" 
+                            />
                         </Grid>
                         <Grid item xs={12} sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}>
-                            <Button color="success" variant="contained" onClick={handleClick}>
+                            <Button 
+                                color="success" 
+                                onClick={onHandleSubmitForm}
+                                variant="contained" 
+                            >
                                 Apply changes
                             </Button>
                         </Grid>
