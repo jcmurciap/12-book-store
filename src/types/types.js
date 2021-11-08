fd71eb7f6e9e9c8ad3c663d5ebae54fe99adaf1e
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-08 12:31:56 -0500

    Add new types

--- src/types/types.js
+++ src/types/types.js
@@ -2,5 +2,11 @@
 export const types = {
     uiOpenModal: '[ui] Open modal',
     uiCloseModal: '[ui] Close modal',
-    bookStoreAddBook: '[bookStore] Add book',
+    
+    eventAddBook: '[bookStore] Add book',
+    eventLoaded: '[bookStore] Event loaded',
+    eventSetActive: '[bookStore] Set active event',
+    eventClearActiveEvent: '[bookStore] Clear active event',
+    eventDelete: '[bookStore] Event delete',
+    eventUpdated: '[bookStore] Event updated',
 };
