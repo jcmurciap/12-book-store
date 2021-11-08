abdce1b31b73c21f2dce0c59ee6022c5a7b2501c
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-08 12:29:53 -0500

    docs: Add actions

--- src/actions/bookStore.js
+++ src/actions/bookStore.js
@@ -1,7 +1,47 @@
 
 import { types } from "../types/types";
+import { fetchWithoutToken } from "../helpers/fetch";
 
-export const bookStoreAddBook = (event) => ({
-    type: types.bookStoreAddBook,
+export const eventAddBook = (event) => ({
+    type: types.eventAddBook,
     payload: event,
 });
+
+export const eventStartLoading = ()  => {
+    return async(dispatch) => {
+        try {
+            const resp = await fetchWithoutToken('events');
+            const body = await resp.json();
+            const events = body.events;
+
+            // persisten los datos al refrescar el navegador
+            dispatch(eventLoaded(events))
+        } catch (error) {
+            console.log(error);
+            
+        };
+    };    
+};
+
+export const eventLoaded = (events) => ({
+    type: types.eventLoaded,
+    payload: events
+});
+
+export const eventSetActive = (event) => ({
+    type: types.eventSetActive,
+    payload: event.row,
+});
+
+export const eventClearActiveEvent = () => ({
+    type: types.eventClearActiveEvent,
+});
+
+export const eventDelete = () => ({
+    type: types.eventDelete
+}); 
+
+export const eventUpdated = (event) => ({
+    type: types.eventUpdated,
+    payload: event
+})
