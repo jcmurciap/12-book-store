b0c7db480a9710dc4863572d40f10ec33d755059
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-08 12:31:26 -0500

    Add helpers

---
+++ src/helpers/fetch.js
@@ -0,0 +1,19 @@
+// We should install json web tokens
+
+export const baseUrl = process.env.REACT_APP_API_URL;
+export const fetchWithoutToken = (endpoint, data, method="GET") => {
+    
+    const url = `${baseUrl}/${endpoint}`;
+
+    if (method === 'GET') {
+        return fetch( url );
+    } else {
+        return fetch(url, {
+            method,
+            headers: {
+                'Content-type': 'application/json', // WHAT IS THAT?
+            },
+            body: JSON.stringify(data),
+        });
+    };
+};
