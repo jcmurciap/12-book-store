096efb45ba8ba228819634d8d4080b3b8608b2d9
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-12 16:30:07 -0500

    Create new book event on client side and save it on the database

--- src/actions/bookStore.js
+++ src/actions/bookStore.js
@@ -1,4 +1,28 @@
+import { fetchWithToken } from "../helpers/fetch";
 import { types } from "../types/types";
+
+const eventStartAddBook = (event) => {
+    
+    return async(dispatch, getState) => {
+        const { uid, name } = getState().auth; // last value returned by the store's reducer
+        try {
+            const resp = await fetchWithToken('events', event, 'POST');
+            const body = await resp.json();
+
+            if(body.ok) {
+                event.id = body.event.id
+                event.user = {
+                    _id: uid,
+                    name: name
+                }; 
+                console.log(event);
+                dispatch(eventAddBook(event));
+            };
+        } catch ( error ){ 
+            console.log(error);
+        };
+    };
+};
 
 export const eventAddBook = (event) => ({
     type: types.eventAddBook,
