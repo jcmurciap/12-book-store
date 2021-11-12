52bd737ccc6203c11ddd189cfa710afacea1b367
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-12 18:06:36 -0500

    Style: quit unused method

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
     Redirect
   } from "react-router-dom";
 import { BookStoreScreen } from '../components/bookStore/BookStoreScreen';
